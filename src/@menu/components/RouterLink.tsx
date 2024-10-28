7f15cd0d36b7708ea0d3793346012e955bd8890d
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-10-28 15:53:23 +0500

    INIT

---
+++ src/@menu/components/RouterLink.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+// React Imports
+import { forwardRef } from 'react'
+
+// Next Imports
+import Link from 'next/link'
+import type { LinkProps } from 'next/link'
+
+// Type Imports
+import type { ChildrenType } from '../types'
+
+type RouterLinkProps = LinkProps &
+  Partial<ChildrenType> & {
+    className?: string
+  }
+
+export const RouterLink = forwardRef((props: RouterLinkProps, ref: any) => {
+  // Props
+  const { href, className, ...other } = props
+
+  if (href == undefined) {
+    return <h1>Hellow</h1>
+  } else {
+    return (
+      <Link ref={ref} href={href} className={className} {...other}>
+        {props.children}
+      </Link>
+    )
+  }
+})
