4d7cc2f839909d4263805f099f561d6f3f3d243a
Muhammad Ahsan <53137713+ahsansoftengineer@users.noreply.github.com>
2024-10-30 16:03:19 +0500

    SETTING UP THEME

--- src/components/layout/vertical/FooterContent.tsx
+++ src/components/layout/vertical/FooterContent.tsx
@@ -7,14 +7,14 @@ import Link from 'next/link'
 import classnames from 'classnames'
 
 // Hook Imports
-import useVerticalNav from '@menu/hooks/useVerticalNav'
+// import useVerticalNav from '@menu/hooks/useVerticalNav'
 
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
 const FooterContent = () => {
   // Hooks
-  const { isBreakpointReached } = useVerticalNav()
+  // const { isBreakpointReached } = useVerticalNav()
 
   return (
     <div
@@ -24,11 +24,11 @@ const FooterContent = () => {
         <span>{`© ${new Date().getFullYear()}, Made with `}</span>
         <span>{`❤️`}</span>
         <span>{` by `}</span>
-        <Link href='https://themeselection.com' target='_blank' className='text-primary'>
-          ThemeSelection
+        <Link href='https://www.starbazaar.pk' target='_blank' className='text-primary'>
+          Starbazaar
         </Link>
       </p>
-      {!isBreakpointReached && (
+      {/* {!isBreakpointReached && (
         <div className='flex items-center gap-4'>
           <Link href='https://themeselection.com/license' target='_blank' className='text-primary'>
             License
@@ -47,7 +47,7 @@ const FooterContent = () => {
             Support
           </Link>
         </div>
-      )}
+      )} */}
     </div>
   )
 }
